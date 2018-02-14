bc063ba38bdd16666eadb2f11f5b29389d4cdb74
Jared Castaneda <35988041+castjare@users.noreply.github.com>
2018-02-14 16:45:49 -0600

    Add error handling to the client app

--- 06Module/client.js
+++ 06Module/client.js
@@ -7,13 +7,21 @@ var body = ''
 http.get(url, (response) => {
 
     response.setEncoding('utf8')
-    
+
     response.on('data', (chunk) => {
         body += chunk
         //console.log(body)
     })
     response.on('end', () => {
-        var jsonData = JSON.parse(body)
-        console.log(jsonData.films[0])
+
+        try {
+            var jsonData = JSON.parse(body)
+            console.log(jsonData.films[0])
+        } catch (error) {
+            console.log(`Formatting error: ${error.message}`)
+        }
+        
     })
+}).on('error', (e) => {
+    console.log(`Got error: ${e.message}`)
 })
