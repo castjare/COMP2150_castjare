1dd29b27103205415153222d7a2859c635eaee62
Jared Castaneda <35988041+castjare@users.noreply.github.com>
2018-03-14 16:47:55 -0500

    Add HTTP methods routing to the server

--- 09Module/message_server.js
+++ 09Module/message_server.js
@@ -1,27 +1,83 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    // Use destructuring operator
-    const {headers, method, url} = req;
-    // Declare a block-scope variable using "let"
-    let body = [];
+    let requestBody = [];
 
     req.on('error', (err) => {
         console.error(err);
     }).on('data', (chunk) => {
-        body.push(chunk);
+        requestBody.push(chunk);
     }).on('end', () => {
-        body = Buffer.concat(body).toString();
+        requestBody = Buffer.concat(requestBody).toString();
 
-        res.on('error', (err) => {
-            console.error(err);
-        });
+        switch (req.method) {
+            case 'POST': 
+            fs.appendFile('./data', requestBody + "\n", (err) => {
+                console.log('write finished', err);
+                res.writeHead(200, {'Content-Type' : 'text/plain'});
+                res.body = 'Message saved!';
+                res.write(res.body);
+                res.end();
+            });
+                
+                break;
 
-        res.writeHead(200, {'Content-Type' : 'application/json'});
-        const responseBody = { body };
-        res.write(JSON.stringify(responseBody));
-        res.end();
+            case 'GET':
+            fs.readFile('./data', 'utf-8', (err, data) => {
+                let dataArr = data.split("\n");
+                res.writeHead(200, {'Content-Type' : 'text/plain'});
+                res.body = dataArr.toString();
+                res.write(res.body);
+                res.end();
+            });
+
+            break;
+
+            case 'PUT':
+            fs.readFile('./data', 'utf-8', (err, data) => {
+                let dataArr = data.split("\n");
+                //Replace the element of array based on the number from the request's url
+                //take the incoming url and parse it into an Url object which can extract just portions of the url we need
+                let incoming_url = url.parse(req.url, true);
+                //extract the url portion
+                let u = incoming_url.pathname;
+                //Remove "/" from the path name
+                u = u.slice(1,);
+                let elToReplace = parseInt(u);
+                if(elToReplace < dataArr.length) {
+                    dataArr[elToReplace] = requestBody;
+                    //Delete current's file data by overwriting it
+                    fs.writeFile('./data', '', (err) => {
+                        console.error(err);
+                    });
+                    dataArr.forEach(element => {
+                        fs.appendFile('./data', element + "\n", (err) => {
+                            console.error(err);
+                        });
+                    });
+                    res.writeHead(200, {'Content-Type' : 'text/plain'});
+                    res.body = "Resource replaced.";
+                    res.write(res.body);
+                    res.end();
+                }
+            });
+
+            break;
+
+            default:
+                res.writeHead(405, {'Content-Type' : 'text/plain'});
+                res.body = "Method not supported.";
+                res.write(res.body);
+                res.end();
+                break;
+        }
 
     });
+
+    res.on('error', (err) => {
+        console.error(err);
+    })
 
 }).listen(8000);
