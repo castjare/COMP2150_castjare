124096e5aa1d3aa7bf9b0823a9bfc1345415d524
Jared Castaneda <35988041+castjare@users.noreply.github.com>
2018-03-28 16:56:05 -0500

    Set up connection to mLab using mongoose

--- webApp/routes/users.js
+++ webApp/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* Display text 'You are cool!' when loading /users/cool/ */
+router.get('/cool', function(req, res, next) {
+  res.send('You are cool!');
+});
+
 module.exports = router;
