8b0f1af8affcd954a16dead02ed16c0e8dff9ec2
Jared Castaneda <35988041+castjare@users.noreply.github.com>
2018-04-02 16:51:00 -0500

    Add async module and script to populate our DB.

--- webApp/models/bookinstance.js
+++ webApp/models/bookinstance.js
@@ -1,12 +1,11 @@
-var mongoose = require ('mongoose');
+var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema({
     book: {type: Schema.ObjectId, ref: 'Book', required: true},
     imprint: {type: String, required: true},
-    status: {type: String, required: true, enum: ['Available', 'Maintenance', 
-'Loaned', 'Reserved'], default: 'Maintenance’},
+    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
     due_back: {type: Date, default: Date.now}
 });
 
